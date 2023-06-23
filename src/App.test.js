c73c45dc7efa787e51dabe3145369928f97e3c13
Matthew Flesher <atlanticcitytc2@gmail.com>
2023-06-23 19:10:29 -0400

    updated test

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const introElement = screen.getByText(/Hi, i'm matthew/i);
+  const introElement = screen.getByText(/Hi, i'm matt/i);
   expect(introElement).toBeInTheDocument();
 });
