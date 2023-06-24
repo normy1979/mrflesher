ad5d21d8b11e352177c6f25bfd52a8038327346e
Matthew Flesher <atlanticcitytc2@gmail.com>
2023-06-24 06:14:41 -0400

    updated css

--- src/components/About.js
+++ src/components/About.js
@@ -4,6 +4,7 @@ export default function About() {
     return ( 
     <>
     <div className="about-div" id="abt">
+        <br/>
         <h3>ABOUT ME</h3>
         <p><em>Since I was a kid growing up in Hawaii, I've been interested in technology.  I've always been curious about things I don't understand.  I tried to teach myself coding when I turned 20, but I didn't do too well.  Back in 2012 I finally decided that I was going to do what I wanted to do and went back to school for a Computer Science degree.  Since then I've had the pleasure of making web applications for the FAA and a non-profit cat rescue.  Also, my curiosity back to school again to get a Master's degree in Computer Engineering.</em></p>
         <p><em>Most of my work is focused on automation, both at work and at home.  I've automated CI/CD pipelines, testing, and even some of my wifes tedious tasks at her job.  However, I still love to learn and create on the web.</em></p>
@@ -14,6 +15,7 @@ export default function About() {
             <dd>Trust the flow.</dd>
             <dd>Keep moving forward.</dd>
         </dl>
+        <br/>
     </div>
     </>
     );
--- src/components/Work.js
+++ src/components/Work.js
@@ -5,20 +5,36 @@ export default function Work() {
     <>
     <div className="work-div" id="work">
         <h3>MY WORK</h3>
+        <br/>
         <div>
-            <a href="https://thekraziestkatlady.com"><img className="work-img" src={require('../assets/images/kkl_logo.png')} alt="kkl"/></a>
-            <h5><em><a href="https://thekraziestkatlady.com">thekraziestkatlady.com</a><p> (A NJ Non-profit Cat Rescue)</p></em></h5>
+            <a href="https://thekraziestkatlady.com"><img className="work-img" src={require('../assets/images/kkl_logo.png')} alt="kkl"/>
+            </a>
+            <h5><em>
+                <a href="https://thekraziestkatlady.com">thekraziestkatlady.com</a>
+                <p>A NJ Non-profit Cat Rescue </p>
+                <p>***</p>
+                <p>Angular | AWS - S3, Lambda, DynamoDB, IAM | NodeJS | HTML | CSS | JavaScript | Gitlab - Issue tracking, Git Version Control, CI/CD</p>
+            </em></h5>
         </div>
+        <br/>
         <br/>
         <div>
             <a href="https://mrflesher.com"><img className="work-img" src={require('../assets/images/blue_honu.png')} alt="honu"/></a>
-            <h5><em><a href="https://mrflesher.com">mrflesher.com</a> <p>(this site)</p></em></h5>
+            <h5><em>
+                <a href="https://mrflesher.com">mrflesher.com</a> 
+                <p>(this site)</p>
+                <p>***</p>
+                <p>ReactJS | Google Cloud Platform (GCP) - Bucket Storage, IAM | AWS - Lambda, IAM | NodeJS | HTML | CSS | GitHub - Issue tracking, Git Version Control, Github Actions</p>
+            </em></h5>
         </div>      
         <br/>     
         <div>
             <a href="http://actunaclub.com"><img className="work-img" src={require('../assets/images/tc_logo.png')} alt="tuna club"/></a>
-            <h5><em><a href="http://actunaclub.com">actunaclub.com</a>
-            <p>(An informational site about our former short term rental property)</p>
+            <h5><em>
+                <a href="http://actunaclub.com">actunaclub.com</a>
+                <p>(An informational site about the history of the property we live on)</p>
+                <p>***</p>
+                <p>Angular | AWS - S3 | HTML | CSS | JavaScript | Gitlab - Issue tracking, Git Version Control, CI/CD</p>
             </em></h5>
         </div>
     </div>
